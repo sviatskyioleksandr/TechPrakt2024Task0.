8ea53e54b49edc99d719b5df8c832a7323ab2665
Олександр Св`ятський <ipz224_som@student.ztu.edu.ua>
2024-02-11 10:43:14 +0200

    Created gitignore, ConsoleApp , temporary.txt

---
+++ ConsoleAppMath/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        try
+        {
+            Console.WriteLine("Введіть перше число:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введіть друге число:");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+            Console.WriteLine($"Сума двох чисел: {sum}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Некоректний ввід. Будь ласка, введіть числа коректно.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Переповнення. Будь ласка, введіть менші числа.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+
+        Console.ReadLine();
+    }
+}
